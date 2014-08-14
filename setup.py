b12a33584588b3af5dd43cffb5dba867d2d69490
Mikael Kohlmyr <mikael.kohlmyr@triggeredmessaging.com>
2014-08-14 15:30:31 +0100

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='utfhate',
-      version='1.0.1',
+      version='1.0.2',
       author='Mikael Kohlmyr',
       author_email='mikael@kohlmyr.com',
       license='LICENSE',
