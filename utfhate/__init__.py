6ce38ccf9760a5aeeffbc2f2edeb4a6379588226
Mikael Kohlmyr <mikael.kohlmyr@triggeredmessaging.com>
2014-08-14 15:05:29 +0100

    added support for unicode strings with unicode characters or code points

--- utfhate/__init__.py
+++ utfhate/__init__.py
@@ -1,3 +1,4 @@
+import six
 import lib
 
 def htmlstring(chars, lazy=False, literal=False):
@@ -7,17 +8,30 @@ def htmlstring(chars, lazy=False, literal=False):
 
     def generator():
         char = None
+
         while True:
             char = char or chars.next()
+            bytestring = isinstance(char, six.binary_type)
             decimal = ord(char)
 
-            if decimal in xrange(192, 256):
+            if bytestring and decimal in xrange(192, 256):
                 entity, char = lib.multipart(char, chars)
                 yield entity
                 continue
-            elif literal and decimal in [60, 62]:
-                yield lib.utfentity(decimal)
+                pass
+            elif decimal > 127 or (literal and decimal in [60, 62]):
+                if bytestring:
+                    yield lib.utfentity(decimal)
+                    pass
+                else:
+                    yield lib.htmlentity(decimal)
+                    pass
+                pass
             else:
                 yield char
+                pass
             char = None
+            pass
+        pass
     return generator() if lazy else ''.join(list(generator()))
+
--- utfhate/lib.py
+++ utfhate/lib.py
@@ -4,6 +4,7 @@ def continuation(decimal):
 def binarycp(*args):
     if not len(args):
         raise ValueError('UTF-8 continuation sequence too short')
+        pass
     elif len(args) == 1:
         return [byteliteral(args[0])[-7:]]
     elif len(args) < 7:
@@ -11,6 +12,7 @@ def binarycp(*args):
         return [byteliteral(args[0])[-leadbits:]] + map(continuation, args[1:])
     else:
         raise ValueError('UTF-8 continuation sequence too long')
+        pass
     return
 
 def decimalcp(*args):
@@ -33,9 +35,11 @@ def multipart(char, iterable):
     while True:
         try:
             char = iterable.next()
+            pass
         except StopIteration:
             char = None
             break
+            pass
 
         decimal = ord(char)
 
@@ -43,6 +47,8 @@ def multipart(char, iterable):
             bytes.append(decimal)
         else:
             break
+            pass
+        pass
     entity = utfentity(*bytes)
 
     return (entity, char)
